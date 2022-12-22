33779d44a08f721f080479fbf7e600afe38a1831
Juan Alberto Virrey Cerriteño <av@iseeyoutech.com>
2022-12-21 18:26:48 -0800

    Iconos, Scroll View y configuración del scroll view

--- src/Routes/AdminRouter.jsx
+++ src/Routes/AdminRouter.jsx
@@ -7,6 +7,7 @@ import TabbarBackground from "../../assets/icons/TabbarBackground"
 import { Box, Icon, IconButton, HStack, VStack } from "native-base";
 import Dashboard from "../Components/Admin/Dashboard"
 import Octicons from "react-native-vector-icons/Octicons";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 
 const BottomStack = createBottomTabNavigator()
@@ -18,7 +19,7 @@ export default function App() {
         tabBar={() => {
             return <Box bg={"transparent"} w="100%" height="120" m={0} p={0} style={{ position: "absolute", bottom: 0 }}>
                 <TabbarBackground width="100%" height="120" style={{ flex: 1, position: "absolute", bottom: 0 }} />
-                {/* <VStack
+                <VStack
                     alignItems="center"
                     bg={{
                         linearGradient: {
@@ -29,7 +30,7 @@ export default function App() {
                     }}
                     mx="auto"
                     w={70} h={70} borderRadius={100} >
-                    <Icon as={Octicons} m="auto" name="plus" size="xl" color="white" />
+                    <Icon as={AntDesign} m="auto" name="plus" size="xl" color="white" />
                 </VStack>
 
                 <HStack flex="1">
@@ -49,7 +50,7 @@ export default function App() {
                         <Icon as={Octicons} name="bell-fill" size="xl" color="marine.900" />
                         <Box>Home</Box>
                     </VStack>
-                </HStack> */}
+                </HStack>
             </Box>
         }}>
         <BottomStack.Screen name={"Dashboard"} component={Dashboard} />
